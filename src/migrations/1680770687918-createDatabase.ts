bff7dbf5d8572fa5757e001b1952ed5b858f4b2d
Flavia Monteiro <flavinhabm@gmail.com>
2023-04-06 05:45:41 -0300

    fix : migrations

--- src/migrations/1680770687918-createDatabase.ts
+++ src/migrations/1680770687918-createDatabase.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class createDatabase1680749251587 implements MigrationInterface {
-    name = 'createDatabase1680749251587'
+export class createDatabase1680770687918 implements MigrationInterface {
+    name = 'createDatabase1680770687918'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "firstName" character varying(45) NOT NULL, "lastName" character varying(45) NOT NULL, "email" character varying(45) NOT NULL, "phone" character varying(11) NOT NULL, "password" character varying(120) NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
