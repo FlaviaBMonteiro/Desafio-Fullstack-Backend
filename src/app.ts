e37c33cb6cf9087e9c27a447e62c473e9886092c
Flavia Monteiro <flavinhabm@gmail.com>
2023-04-10 09:50:48 -0300

    fix: head autorizathion

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,11 @@ import { handleErrors } from "./errors"
 import userRoutes from "./routers/users.routes"
 import loginRoutes from "./routers/login.routes"
 import contactRoutes from "./routers/contacts.routes"
+import cors from "cors"
 
 const app: Application = express()
 app.use(express.json())
-
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
 app.use("/users", userRoutes)
 app.use("/login", loginRoutes)
 app.use("/contacts", contactRoutes)
